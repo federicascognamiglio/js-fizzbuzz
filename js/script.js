4e343e8ae5d0082a7ab88019f82681247af2b481
Federica Scognamiglio <federicascognamiglio@Federicas-Laptop.local>
2024-10-29 15:51:18 +0100

    feat: exercise completed

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ const multipleFive = "Buzz";
 // ESECUZIONE LOGICA
 let multiples;
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 === 0) {
-       multiples = multipleThree;
-    }  else if (i % 5 === 0) {
-        multiples = multipleFive;
-    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
         multiples = multipleThree + multipleFive;
+    }  else if (i % 3 === 0) {
+        multiples = multipleThree;
+    } else if (i % 5 === 0) {
+        multiples = multipleFive;
     } else {
         multiples = i;
     }
+    // OUTPUT
     console.log(multiples);
 }
